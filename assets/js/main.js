da0732cc8440b7bee97bace5de4016b41d060dd8
Martin <martineprocopio2020@gmail.com>
2022-11-02 19:14:36 -0300

    Se agrega js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-
+window.addEventListener("load", function(){
+    /*console.log("Hola mundo");*/
+    
+    /*var hola = document.getElementsByClassName("bottons")[0];
+    console.log(hola);*/
+    /*var a = document.getElementById("guardar").innerHTML="bienvenidos";
+    console.log(a);*/
+});
